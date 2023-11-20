68dc2ca3334efd312a652afda598f5c3b8728a5d
BjorneX <51091558+BjorneX@users.noreply.github.com>
2023-11-20 18:38:27 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,35 @@
+import * as THREE from "three";
+
+//3 basics
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const canvas = document.querySelector("#c");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.CapsuleGeometry(1, 1.5, 4, 8);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const capsule = new THREE.Mesh(geometry, material);
+scene.add(capsule);
+
+camera.position.z = 5;
+
+document.querySelector("body").addEventListener("click", animate);
+
+let cars = ["Bmw"];
+
+function animate() {
+  requestAnimationFrame(animate);
+  capsule.rotation.x += 0.01;
+  capsule.rotation.y += 0.01;
+  cars.push();
+  renderer.render(scene, camera);
+}
+animate();
